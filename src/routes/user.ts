9f29838f9c7ab3fa05717de876b49c53f2ccc052
Shubham Parihar <shubhamparihar391@gmail.com>
2021-07-31 20:23:38 +0530

    feat(routes): add get multiple users and delete all users endpoints

--- src/routes/user.ts
+++ src/routes/user.ts
@@ -24,7 +24,7 @@ const userRoutes: FastifyPluginCallback = async fastify => {
   });
 
   /**
-   * Get a single user
+   * Get a single user by id
    */
   fastify.route<{ Params: getUserParamsType }>({
     method: 'GET',
@@ -37,8 +37,39 @@ const userRoutes: FastifyPluginCallback = async fastify => {
     },
     handler: async function (req, reply) {
       const { id } = req.params;
-      const user = await this.mongo.db?.collection('users').findOne({ id });
+      const user = await this.mongo.db?.collection('users').findOne({ id }, { projection: { _id: 0 } });
       reply.send(user);
+    },
+  });
+
+  /**
+   * Get multiple users by ids
+   */
+  fastify.route<{ Querystring: { ids: string } }>({
+    method: 'GET',
+    url: '/users',
+    schema: {
+      querystring: {
+        type: 'object',
+        properties: {
+          ids: { type: 'string' },
+        },
+        required: ['ids'],
+      },
+      response: {
+        200: {
+          type: 'array',
+          items: User,
+        },
+      },
+    },
+    handler: async function (req, reply) {
+      const userIds = req.query.ids.split(',');
+      const users = await this.mongo.db
+        ?.collection('users')
+        .find({ id: { $in: userIds } }, { projection: { _id: 0 } })
+        .toArray();
+      reply.send(users);
     },
   });
 
@@ -47,7 +78,7 @@ const userRoutes: FastifyPluginCallback = async fastify => {
    */
   fastify.route({
     method: 'GET',
-    url: '/users',
+    url: '/users/all',
     schema: {
       response: {
         200: {
@@ -62,6 +93,18 @@ const userRoutes: FastifyPluginCallback = async fastify => {
         .find({}, { projection: { _id: 0 } })
         .toArray();
       reply.send(users);
+    },
+  });
+
+  /**
+   * Delete all users
+   */
+  fastify.route({
+    method: 'DELETE',
+    url: '/users/all',
+    handler: async function (req, reply) {
+      await this.mongo.db?.collection('users').deleteMany({});
+      reply.status(204).send();
     },
   });
 };
