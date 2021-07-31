2dced959e4cfe35796ab10529fb1037ab3410d4c
Shubham Parihar <shubhamparihar391@gmail.com>
2021-07-31 13:36:26 +0530

    feat(routes): add endpoint for fetching all users

--- src/routes/user.ts
+++ src/routes/user.ts
@@ -3,23 +3,29 @@ import { v4 as uuidv4 } from 'uuid';
 import { getUserParams, getUserParamsType, NewUser, NewUserType, User } from '../typings/user.js';
 
 const userRoutes: FastifyPluginCallback = async fastify => {
+  /**
+   * Create a new user
+   */
   fastify.route<{ Body: NewUserType }>({
     method: 'POST',
     url: '/users',
     schema: {
       body: NewUser,
       response: {
-        200: User,
+        201: User,
       },
     },
     handler: async function (req, reply) {
       const { body } = req;
       const newUser = { ...body, id: uuidv4() };
       await this.mongo.db?.collection('users').insertOne(newUser);
-      reply.send(newUser);
+      reply.status(201).send(newUser);
     },
   });
 
+  /**
+   * Get a single user
+   */
   fastify.route<{ Params: getUserParamsType }>({
     method: 'GET',
     url: '/users/:id',
@@ -33,6 +39,29 @@ const userRoutes: FastifyPluginCallback = async fastify => {
       const { id } = req.params;
       const user = await this.mongo.db?.collection('users').findOne({ id });
       reply.send(user);
+    },
+  });
+
+  /**
+   * Get all users
+   */
+  fastify.route({
+    method: 'GET',
+    url: '/users',
+    schema: {
+      response: {
+        200: {
+          type: 'array',
+          items: User,
+        },
+      },
+    },
+    handler: async function (req, reply) {
+      const users = await this.mongo.db
+        ?.collection('users')
+        .find({}, { projection: { _id: 0 } })
+        .toArray();
+      reply.send(users);
     },
   });
 };
