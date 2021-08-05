26eac8af6b1314e0304bcdb7b040c15977911709
Shubham Parihar <shubhamparihar391@gmail.com>
2021-08-05 21:19:05 +0530

    feat: add fastify-cors dependency

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 import Fastify from 'fastify';
 import autoLoad from 'fastify-autoload';
+import cors from 'fastify-cors';
 import fastifyMongodb from 'fastify-mongodb';
 import { dirname, join } from 'path';
 import { fileURLToPath } from 'url';
@@ -12,6 +13,10 @@ const fastify = Fastify();
 fastify.register(fastifyMongodb, {
   forceClose: true,
   url: process.env.BLOGSBOOK_ATLAS_URI,
+});
+
+fastify.register(cors, {
+  origin: '*',
 });
 
 fastify.register(autoLoad, {
